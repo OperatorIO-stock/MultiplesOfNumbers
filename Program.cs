5f236f7c2c54b30503b0a12cc59d9192f8d86881
operatorIO-stock <vova.chigorin@yandex.com>
2024-12-02 20:48:23 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace MultiplesOfNumbers;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int numberFirst = 10;
+        int numberLast = 25;
+
+        for (int i = numberFirst; i <= numberLast; i++)
+        {
+            Console.WriteLine(i);
+        }
+
+        Console.WriteLine();
+
+        numberFirst = 50;
+        numberLast = 150;
+
+        for (int i = numberFirst; i <= numberLast; i++)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
